c68a34d4aa11e4815995246c964d837c2709d3d8
ST10505924 <ST10505924@myemeris.edu.za>
2026-03-17 16:30:02 +0200

    Goofy Chat2

--- src/main/java/com/mycompany/goofychat/GoofyChat.java
+++ src/main/java/com/mycompany/goofychat/GoofyChat.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.goofychat;
-import java.util.Scanner;
+import java.util.*;
 /**
  *
  * @author lab_services_student
@@ -12,6 +12,21 @@ public class GoofyChat {
 
     public static void main(String[] args) {
         
+        Scanner scan = new Scanner(System.in);
+        
+        //Welcoming user to the application
+        System.out.println("**********HELLO, WELCOME TO GOOFY CHAT!********** \n Do you have an account?If not, Create your account.");
+        
+        //Prompting user for personal credentials
+        System.out.print("\nEnter First Name: ");
+        String firstName = scan.nextLine();
+        
+        System.out.print("\nEnter Last Name: ");
+        String lastName = scan.nextLine();
+        
+        System.out.print("\nEnter Date Of Birth: ");
+        String birthDate = scan.nextLine();
+      
         
     }
 }
---
+++ src/main/java/com/mycompany/goofychat/Login.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.goofychat;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Login {
+    
+    
+    
+}
