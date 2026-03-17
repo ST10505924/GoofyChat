67ebc55be3bc72efc548db5eb2fadc699a09317c
ST10505924 <ST10505924@myemeris.edu.za>
2026-03-17 15:00:31 +0200

    Goofy Chat

---
+++ src/main/java/com/mycompany/goofychat/GoofyChat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.goofychat;
+import java.util.Scanner;
+/**
+ *
+ * @author lab_services_student
+ */
+public class GoofyChat {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
